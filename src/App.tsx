882bb4e8e357dd0cc88657b47ee5372ee1755a39
Yang Zhao <yangzhao.nz@gmail.com>
2022-06-23 12:23:22 +1200

    Initialize with Yarn 3, CRA, TS and styled-C

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import logo from "./logo.svg";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <AppContainer>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <AppLink
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </AppLink>
+      </AppHeader>
+    </AppContainer>
+  );
+}
+
+export default App;
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: App-logo-spin infinite 20s linear;
+  }
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
